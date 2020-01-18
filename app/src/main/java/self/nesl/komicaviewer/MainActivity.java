8e47eaae53853b9c3445b360e2aedd89561c4b6a
neslxzhen <neslxzhen@gmail.com>
2020-01-18 20:05:27 +0800

    feat: navigation drawer activity

--- app/src/main/java/self/nesl/komicaviewer/MainActivity.java
+++ app/src/main/java/self/nesl/komicaviewer/MainActivity.java
@@ -1,14 +1,76 @@
 package self.nesl.komicaviewer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // fab
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // drawer
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_slideshow,
+                R.id.nav_history,
+                R.id.nav_favorite,
+
+                R.id.nav_home,
+                R.id.nav_board
+        )
+                .setDrawerLayout((DrawerLayout)findViewById(R.id.drawer_layout))
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        // add host item in there
+//        Menu boardMenu=navigationView.getMenu().addSubMenu("host");
+//        for(Host host:getHosts()){
+//            addMenu( boardMenu,host.getIcon(), host);
+//        }
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
