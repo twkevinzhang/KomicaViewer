72053a2394b21570b51fc2bc173494573d5d12ce
neslxzhen <neslxzhen@gmail.com>
2020-08-13 19:39:54 +0800

    Use WebView

--- app/src/main/java/self/nesl/komicaviewer/adapter/PostlistAdapter.java
+++ app/src/main/java/self/nesl/komicaviewer/adapter/PostlistAdapter.java
@@ -1,11 +1,22 @@
 package self.nesl.komicaviewer.adapter;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LevelListDrawable;
+import android.os.AsyncTask;
 import android.text.Html;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +27,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
+import org.jsoup.nodes.Element;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
@@ -24,7 +42,6 @@ import java.util.Set;
 import self.nesl.komicaviewer.R;
 import self.nesl.komicaviewer.model.Picture;
 import self.nesl.komicaviewer.model.Post;
-
 import static self.nesl.komicaviewer.util.Utils.print;
 
 public class PostlistAdapter extends RecyclerView.Adapter<PostlistAdapter.PostlistViewHolder> {
@@ -39,23 +56,21 @@ public class PostlistAdapter extends RecyclerView.Adapter<PostlistAdapter.Postli
     }
 
     public class PostlistViewHolder extends RecyclerView.ViewHolder{
-        private ImageView imgThumb;
-        private ImageView imgOri;
         private TextView txtPostId;
         private TextView txtPostInd;
         private TextView txtReplyCount;
         private TextView txtPoster;
         private TextView txtTime;
+        private WebView webView;
 
         PostlistViewHolder(View v) {
             super(v);
-            imgThumb = v.findViewById(R.id.imgThumb);
-            imgOri = v.findViewById(R.id.imgOri);
             txtTime = v.findViewById(R.id.txtTime);
-            txtPostInd = v.findViewById(R.id.txtInd);
             txtReplyCount = v.findViewById(R.id.txtReplyCount);
             txtPostId = v.findViewById(R.id.txtId);
             txtPoster = v.findViewById(R.id.txtPoster);
+            webView=v.findViewById(R.id.webView);
+//            itemView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
         }
     }
 
@@ -68,7 +83,7 @@ public class PostlistAdapter extends RecyclerView.Adapter<PostlistAdapter.Postli
     }
 
     @Override
-    public void onBindViewHolder(@NonNull final PostlistViewHolder holder, final int i) {
+    public void onBindViewHolder(@NonNull PostlistViewHolder holder, final int i) {
         final Post post = postlist.get(i);
 
         // other
@@ -76,54 +91,28 @@ public class PostlistAdapter extends RecyclerView.Adapter<PostlistAdapter.Postli
         holder.txtReplyCount.setText("回應:" + post.getReplyCount());
         holder.txtPoster.setText(post.getPoster());
         holder.txtTime.setText(post.getTimeStr());
-        holder.imgThumb.setVisibility(View.VISIBLE);
-        holder.imgOri.setVisibility(View.GONE);
 
-        // imgThumb & imgOri
-        ArrayList<Picture> pics = post.getPics();
-        String thumbUrl="",picUrl="";
-        if(pics.size() != 0){
-            Picture pic=pics.get(0);
-            thumbUrl = pic.getThumbnailUrl();
-            picUrl =pic.getOriginalUrl();
-        }
-
-        // 通過tag來防止錯位、忽大忽小
-        holder.imgThumb.setTag(R.id.imageid, thumbUrl);
-        if (holder.imgThumb.getTag(R.id.imageid).equals(thumbUrl)) {
-            Glide.with(holder.imgThumb.getContext())
-                    .load(thumbUrl)
-                    .fitCenter()
-                    .into(holder.imgThumb);
-
-            String finalPicUrl = picUrl;
-            holder.imgThumb.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    holder.imgThumb.setVisibility(View.GONE);
-                    holder.imgOri.setVisibility(View.VISIBLE);
-
-                    Glide.with(holder.imgOri.getContext())
-                            .load(finalPicUrl)
-                            .fitCenter()
-                            .into(holder.imgOri);
-                }
-            });
-        }
-
-        // itemView
-        View.OnClickListener onClickListener=new View.OnClickListener() {
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(callBack!=null)callBack.itemOnClick(post);
+                callBack.itemOnClick(post);
             }
-        };
-        holder.itemView.setOnClickListener(onClickListener);
+        });
 
-        // txtPostInd
-        holder.txtPostInd.setText(Html.fromHtml(post.getQuoteElement().html()));
-        holder.txtPostInd.setOnClickListener(onClickListener);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        fragment.getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int width = displayMetrics.widthPixels;
 
+        // todo
+//        int width=holder.itemView.getWidth();
+//        print(this,"width",width+"");
+
+        Element quoteEle=post.getQuoteElement();
+        for(Element imgEle: quoteEle.select("img")){
+            imgEle.attr("width", (width/3)+"");
+        }
+        holder.webView.setInitialScale(250);
+        holder.webView.loadDataWithBaseURL("",quoteEle.html(),"text/html","UTF-8","");
     }
 
     @Override
