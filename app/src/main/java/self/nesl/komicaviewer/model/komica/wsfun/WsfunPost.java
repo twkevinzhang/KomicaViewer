6477f0b5afd5569d7c6de55839c0321d26681f94
neslxzhen <neslxzhen@gmail.com>
2020-08-15 12:59:41 +0800

    Fixed Wsfun

--- app/src/main/java/self/nesl/komicaviewer/model/komica/wsfun/WsfunPost.java
+++ app/src/main/java/self/nesl/komicaviewer/model/komica/wsfun/WsfunPost.java
@@ -2,6 +2,7 @@ package self.nesl.komicaviewer.model.komica.wsfun;
 
 import self.nesl.komicaviewer.dto.PostDTO;
 import self.nesl.komicaviewer.model.komica.sora.SoraPost;
+import self.nesl.komicaviewer.util.UrlUtils;
 
 public class WsfunPost extends SoraPost {
 
@@ -13,11 +14,15 @@ public class WsfunPost extends SoraPost {
         return (WsfunPost)new WsfunPost(dto).parse();
     }
 
-
     public WsfunPost(PostDTO dto) {
         String[] strs = dto.postId.split(" ");
         setPostId(strs[0]);
         setPostElement(dto.postElement);
         setBoardUrl(dto.boardUrl);
+    }
+
+    @Override
+    public String getDownloadUrl(int page, String boardUrl,String postId) {
+        return boardUrl+"/index.php?res="+postId;
     }
 }
